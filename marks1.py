6d5aaebd898f2dde6e5b304f7b9bfb0906d94e42
plutone247 <83330405+plutone247@users.noreply.github.com>
2021-09-13 16:27:23 +0900

    Delete marks1.py

--- marks1.py
+++
@@ -1,9 +0,0 @@
-marks = [90,25,67,45,80]
-
-number = 0
-for mark in marks:
-    number = number +1
-    if mark >= 60:
-        print("num %d pass" % number)
-    else:
-        print("num %d nopass" % number)
